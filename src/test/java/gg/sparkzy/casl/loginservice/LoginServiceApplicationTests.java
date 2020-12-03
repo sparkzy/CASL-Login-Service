3d862f6a40523b7de86c77f9337a618685fb3db7
sparkzy <31968082+sparkzy@users.noreply.github.com>
2020-12-02 20:55:04 -0500

    created dev branch. added @EnableCircuitBreaker to main.

---
+++ src/test/java/gg/sparkzy/casl/loginservice/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.sparkzy.casl.loginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
