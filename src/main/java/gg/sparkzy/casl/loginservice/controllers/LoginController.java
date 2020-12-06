46f81c8e7c02ddaa5089d4bdebd418f4266de726
sparkzy <31968082+sparkzy@users.noreply.github.com>
2020-12-05 22:18:00 -0500

    mvc to grab users by username

--- src/main/java/gg/sparkzy/casl/loginservice/controllers/LoginController.java
+++ src/main/java/gg/sparkzy/casl/loginservice/controllers/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import gg.sparkzy.casl.loginservice.services.LoginService;
+
 @RestController
 @RequestMapping("/login")
 @RefreshScope // actuator/refresh
